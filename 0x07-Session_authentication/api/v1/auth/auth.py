08d47d3d85e45e16bf56f69ed2fb7a08991d7e81
jgra007 <66275651+jgra007@users.noreply.github.com>
2021-11-09 17:18:02 -0500

    Update auth.py

--- 0x07-Session_authentication/api/v1/auth/auth.py
+++ 0x07-Session_authentication/api/v1/auth/auth.py
@@ -3,6 +3,7 @@
 """
 from flask import request
 from typing import List, TypeVar
+from os import getenv
 
 
 class Auth():
@@ -44,3 +45,14 @@ class Auth():
         current_user function
         """
         return None
+
+    def session_cookie(self, request=None):
+        """
+        Return cookie value from request.
+        """
+        if request is None:
+            return None
+
+        cookie_key = getenv('SESSION_NAME')
+        return request.cookies.get(cookie_key)
+    
